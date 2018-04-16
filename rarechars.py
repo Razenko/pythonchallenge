66dd567617aeda75dc8746f1819393cf8669edb2
Razenko <schoeber@home.nl>
2018-04-16 19:02:51 +0200

    Derp inconsistent codestyle

--- rarechars.py
+++ rarechars.py
@@ -10,7 +10,7 @@ def findchars(file):
     rarefile = open(file, "r")  # open file
     resultlist = list()  # create new list to store filtered results
     for character in list(rarefile.read()):  # read the file and iterate through each individual character
-        if (str.isalpha(character)):  # check if character is a letter
+        if str.isalpha(character):  # check if character is a letter
             resultlist.append(character)  # store results in list
     return "".join(resultlist)  # convert the list to a string and return it
 
