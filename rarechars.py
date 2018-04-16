b37df0d75f2b20c7a7bf180ace89fc035ebbd3cc
Razenko <schoeber@home.nl>
2018-04-16 18:57:05 +0200

    Small refactor to rarechars.py

--- rarechars.py
+++ rarechars.py
@@ -3,15 +3,16 @@
 # These characters are pasted in a separate text file to improve readability (rarechars.txt).
 
 
-rarechars = open("rarechars.txt", "r")  # open file
+filename = "rarechars.txt"
 
 
 def findchars(file):
+    rarefile = open(file, "r")  # open file
     resultlist = list()  # create new list to store filtered results
-    for character in list(file.read()):  # read the file and iterate through each individual character
+    for character in list(rarefile.read()):  # read the file and iterate through each individual character
         if (str.isalpha(character)):  # check if character is a letter
             resultlist.append(character)  # store results in list
     return "".join(resultlist)  # convert the list to a string and return it
 
 
-print(findchars(rarechars))  # print the result
+print(findchars(filename))  # print the result
